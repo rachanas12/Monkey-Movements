a46396056b09d60ec14aa06d7c643f8629ad32c4
rachanas12 <rachanas310796@gmail.com>
2023-04-11 19:27:09 +0530

    hello

---
+++ monkey.js
@@ -0,0 +1,7 @@
+const a=document.getElementById("close")
+
+a.addEventListener("click",()=>{
+    const b=document.getElementById("open")
+    a.style.display="none"
+    b.style.display="block"
+})
